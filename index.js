74030e148866af1e0f02258ad3255004fbbd1a7b
jawwadturabi <memonjawwad9@gmail.com>
2019-04-08 16:31:00 +0500

    second

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const { WebhookClient } = require("dialogflow-fulfillment");
+// const { WebhookClient } = require("dialogflow-fulfillment");
 const app = express().use(bodyParser.json());
 const dburi = "mongodb+srv://author:author123@cluster0-geoiq.mongodb.net/test?retryWrites=true";
 process.env.DEBUG = "dialogflow:debug";
@@ -33,42 +33,73 @@ var userDetail = new mongoose.Schema(
 var model = new mongoose.model("userData", userDetail);
 
 app.post("/webhook", function (request, response, next) {
-    const _agent = new WebhookClient({ request: request, response: response });
+    // const _agent = new WebhookClient({ request: request, response: response });
 
-    function welcome(agent) {
-        agent.add(`Good day! you want to book a room`);
+    var intent = request.body.intent.displayname
+    switch (intent) {
+        case 'Booking':
+
+            Booking(request, response)
+
+            break
+
+
+        case 'Default Welcome Intent':
+
+            welcome(request, response)
+
+            break
     }
 
-    function Booking(agent) {
-        const name = agent.parameters.name;
-        const persons = agent.parameters.persons;
-        const email = agent.parameters.email;
-        console.log(name, persons, email);
-        var data = {
-            name: name,
-            persons: persons,
-            email: email
-        }
-        var saveData = new model(data);
-        saveData.save((err, mydata) => {
-            if (err) {
-                console.log("error is:", err);
-                agent.add(`Error while writing on database`);
-            } else {
-                agent.add(`${name} Your request is proceeded to concern department.
-                We'll contact you on this email ${email} Thanks for choosing our Hotel.`);
-                console.log("data is", mydata)
+    function welcome(req, res) {
+        response.send({
+
+            fulfillmentText: `Good day! you want to book a room`
+        })
+    }
+        function Booking(req, res) {
+
+            // const name = agent.parameters.name;
+            // const persons = agent.parameters.persons;
+            // const email = agent.parameters.email;
+            const name = request.body.queryResult.parameters.name;
+            const persons = request.body.queryResult.parameters.persons;
+            const email = request.body.queryResult.parameters.email;
+
+
+            console.log(name, persons, email);
+            var data = {
+                name: name,
+                persons: persons,
+                email: email
             }
-        });
-        console.log("data of :", data)
-    }
-    let intentMap = new Map();
-    intentMap.set("Default Welcome Intent", welcome);
-    intentMap.set("Booking", Booking);
-    // intentMap.set("Complaint",complaint);
+            var saveData = new model(data);
+            saveData.save((err, mydata) => {
+                if (err) {
+                    console.log("error is:", err);
+                    response.send({
 
-    _agent.handleRequest(intentMap);
-});
+                        fulfillmentText: `Error while writing on database`
+                    })
+                }
+                else {
+                    response.send({
+
+                        fulfillmentText: `${name} Your request is proceeded to concern department.
+                We'll contact you on this email ${email} Thanks for choosing our`
+                    })
+                    console.log("data is", mydata)
+                }
+            });
+            console.log("data of :", data)
+        }
+        // let intentMap = new Map();
+        // intentMap.set("Default Welcome Intent", welcome);
+        // intentMap.set("Booking", Booking);
+        // intentMap.set("Complaint",complaint);
+
+        // _agent.handleRequest(intentMap);
+    });
 app.listen(process.env.PORT || 8088, function () {
     console.log("app is running in 3000");
 })
